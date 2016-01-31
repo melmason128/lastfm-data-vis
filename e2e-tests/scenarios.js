50b2a0faecdea053af3b95f33b7bdfe32149c23a
Mel <melmason128@gmail.com>
2016-01-31 22:57:24 +0000

    Revert travis-ci debugging code

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -3,18 +3,12 @@ describe('LastFMDataVis', function () {
         browser.get('index.html');
         expect(browser.getLocationAbsUrl()).toMatch('/dashboard');
     });
-    it('should run protractor tests without timing out', function () {
-        expect(1).toBe(1);
-    });
     describe('dashboard', function () {
         beforeEach(function () {
             browser.get('index.html#/dashboard');
         });
-        it('should render the dashboard view on /dashboard', function (done) {
-            (element.all(by.css('[ng-view] h3')).first()).getText().then(function (result) {
-                expect(result).toMatch(/Dashboard/);
-                done();
-            });
+        it('should render the dashboard view on /dashboard', function () {
+            expect(element.all(by.css('[ng-view] h3')).first().getText()).toMatch(/Dashboard/);
         });
     });
 });
--- e2e-tests/scenarios.ts
+++ e2e-tests/scenarios.ts
@@ -10,24 +10,14 @@ describe('LastFMDataVis', ()=> {
         expect(browser.getLocationAbsUrl()).toMatch('/dashboard');
     });
 
-
-    it('should run protractor tests without timing out', ()=>{
-        expect(1).toBe(1);
-    });
-
     describe('dashboard', ()=>{
 
         beforeEach(() => {
             browser.get('index.html#/dashboard');
         });
 
-        it('should render the dashboard view on /dashboard', (done)=>{
-            (<any>(element.all(by.css('[ng-view] h3')).first())).getText().then(
-                (result) => {
-                    expect(result).toMatch(/Dashboard/);
-                    done();
-                }
-            );
+        it('should render the dashboard view on /dashboard', ()=>{
+            expect(element.all(by.css('[ng-view] h3')).first().getText()).toMatch(/Dashboard/);
         });
 
 
